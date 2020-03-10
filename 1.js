be88db3d0301bcfb77a1edc220998b197078eab7
TaeyeonzjX <39398985+TaeyeonzjX@users.noreply.github.com>
2020-03-10 18:01:46 +0800

    js工厂创建
    第一次使用

---
+++ 1.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<script type="text/javascript">
+			/*var obj = {
+				name:"孙悟空",
+				age:18,
+				gender:"男",
+				sayName:function(){
+					alert(this.name);
+				}
+			}*/			
+			function createPerson(name,age,gender){
+				var obj = new Object();
+				obj.name = name;
+				obj.age = age;
+				obj.gender = gender;
+				obj.sayName = function(){
+					alert(this.name);
+				}
+				return obj;
+			}
+			
+			var obj3 = createPerson("猪八戒",28,"男");
+			obj3.sayName();
+		</script>
+	</head>
+	<body>
+	</body>
+</html>
